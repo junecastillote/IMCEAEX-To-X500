a85c651de99b59f58228a8c24abd4fc052d1ec34
June Castillote <june.castillote@gmail.com>
2019-07-08 12:40:42 +0800

    Update Convert-IMCEAEXToX500.ps1

--- Convert-IMCEAEXToX500.ps1
+++ Convert-IMCEAEXToX500.ps1
@@ -1,43 +1,5 @@
 
-<#PSScriptInfo
-
-.VERSION 1.0
-
-.GUID 910a560a-27de-4a03-bd5b-16a04d766779
-
-.AUTHOR June Castillote
-
-.COMPANYNAME https://lazyexchangeadmin.com
-
-.COPYRIGHT june.castillote@gmail.com
-
-.TAGS IMCEAEX X500 EXCHANGE EMAIL ADDRESS SCRIPT FUNCTION TOOL
-
-.LICENSEURI 
-
-.PROJECTURI https://github.com/junecastillote/IMCEAEX-To-X500
-
-.ICONURI 
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS 
-
-.EXTERNALSCRIPTDEPENDENCIES 
-
-.RELEASENOTES
-
-
-.PRIVATEDATA 
-
-#>
-
-<# 
-
-.DESCRIPTION 
- Converts IMCEAEX string to a valid X500 Address 
-
-#> 
+Function Convert-IMCEAEXToX500 {
     [cmdletbinding()]
     param (
         [Parameter(Position=0,Mandatory)]
@@ -51,3 +13,4 @@
     $IMCEAEX = ($IMCEAEX).Replace("+29", ")")
     $IMCEAEX = ($IMCEAEX).Replace("+2E", ".")
     Return $IMCEAEX
+}
